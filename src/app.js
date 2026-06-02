b5ef986f34d522e94a48dd0f169cf84b8ecfbaf6
Anjani <anjani682002@gmail.com>
2026-06-02 17:24:07 +0530

    api test

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) =>{
-    res.send("Hello!!!!!!!!!!!!!!!!!!");
+app.get("/user", (req, res) => {
+  res.send({ name: "kunal", age: 23 });
 });
 
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000 sucessfully!");
+app.post("/user", (req, res) => {
+  res.send("data successfuly saved to the database");
+});
+
+let user = {
+  name: "kunal",
+  age: 23,
+};
+
+app.put("/user", (req, res) => {
+  user.name = "Anjani";
+  user.age = 25;
+
+  res.send(user);
+});
+
+app.patch("/user", (req, res) => {
+  user.name = "Jay";
+
+  res.send(user);
+});
+
+app.delete("/user", (req,res) => {
+  res.send("user delete sucessfully!");
+});
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000 sucessfully!");
 });
